e9de49daffed8bd30b5b7e83a4a4a33bd40caed5
Yousef <yousefmouse1@gmail.com>
2026-07-20 22:25:04 +0300

    update

--- browsers-config/browser.nix
+++ browsers-config/browser.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, zen-browser, ... }:
 let
-  wrapFirefoxBrowser = import ./firefox-config.nix { inherit pkgs lib; };
+ #wrapFirefoxBrowser = import ./firefox-config.nix { inherit pkgs lib; };
   unwrapped = zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped;
 in
 {
